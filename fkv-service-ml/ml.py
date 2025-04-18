7117c1e868a7c282f3dc0c7e55acd312aa8a8094
Федор <fedorgym2005@gmail.com>
2025-04-18 09:11:06 +0700

    added json

--- fkv-service-ml/ml.py
+++ fkv-service-ml/ml.py
@@ -1,12 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
 from models import predict_cat
+
 app = FastAPI()
+import json
+
 
 @app.post("/predict")
 async def predict(prompt: str):
-    pr = predict_cat(prompt)
+    data = json.loads(prompt)
+    correct_prompt = data["text"]
+    pr = predict_cat(correct_prompt)
     return {"predict": pr}
+
 
 if __name__ == "__main__":
     uvicorn.run("ml:app", reload=True, host='0.0.0.0', port=8001)
